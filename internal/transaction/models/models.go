24b5a6f7294fa9512e7a584413708d6e9c5bcc6e
Travis Cammack <traviscammack@protonmail.com>
2024-10-03 15:43:56 -0500

    starting to add the transactions stuff

--- internal/transaction/models/models.go
+++ internal/transaction/models/models.go
@@ -1,5 +1,35 @@
 package models
 
-type Transaction struct {
+import (
+	"database/sql"
+	"errors"
+	"log/slog"
+	"time"
 
+	"github.com/jmoiron/sqlx"
+)
+
+type Transaction struct {
+	Id string
+	UserId string `db:"user_id"`
+	AccountId string`db:"account_id"`
+	StripeId string`db:"stripe_id"`
+	Completed bool
+	CreatedAt time.Time`db:"created_at"`
+	UpdatedAt time.Time`db:"updated_at"`
+}
+
+func FetchMany(userId string, db *sqlx.DB) (*[]Transaction, error) {
+	sqls := "SELECT * FROM transactions WHERE user_id = $1"
+	transactions := []Transaction{}
+	err := db.Select(&transactions, sqls, userId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			slog.Warn("Failed to fetch transactions", "user", userId, "error", err.Error())
+			return nil, nil
+		}
+		slog.Error("Failed to fetch transactions", "user", userId, "error", err.Error())
+		return nil, err
+	}
+	return &transactions, nil
 }
