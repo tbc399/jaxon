12c387d6b0c56886bc9dea448a1009f797a8eed0
Travis Cammack <traviscammack@protonmail.com>
2025-02-13 06:39:54 -0600

    fixed the side bar nav to the viewport

--- internal/transaction/templates/transactions-partial_templ.go
+++ internal/transaction/templates/transactions-partial_templ.go
@@ -45,9 +45,9 @@ func transactionsPartial(transactions []interface{}) templ.Component {
 					return templ_7745c5c3_Err
 				}
 				var templ_7745c5c3_Var2 string
-				templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(transaction.(time.Time).Format("Jan 02, 2006"))
+				templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(transaction.(time.Time).Format("January 02, 2006"))
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/transaction/templates/transactions-partial.templ`, Line: 19, Col: 80}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/transaction/templates/transactions-partial.templ`, Line: 19, Col: 84}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 				if templ_7745c5c3_Err != nil {
